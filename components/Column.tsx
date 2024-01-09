2b8285b03db8446922b4cd05cb3d30f61c18a4eb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-09 12:16:00 +0300

    Made some styling updates

--- components/Column.tsx
+++ components/Column.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const Column = ({ state }: Props) => {
 	return (
-		<div className=' flex flex-1 border border-white min-h-[20rem] m-5 rounded-md p-5 w-full'>
+		<div className=' flex flex-1 flex-col items-center border border-white min-h-[20rem] m-5 rounded-md p-5 w-full'>
 			<p>{state}</p>
 			<Task title='Something in the way' />
 		</div>
