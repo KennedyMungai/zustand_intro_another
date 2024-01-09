2344725c6c674892e0ce69e820ea31f1882add20
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-09 11:56:23 +0300

    Defined three columns with three different states

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import Column from '../components/Column'
+
 const App = () => {
   return (
-    <main className="min-h-screen">App</main>
+    <main className="min-h-screen">
+      <Column state="PLANNED" />
+      <Column state="ONGOING" />
+      <Column state="DONE" />
+    </main>
   )
 }
 
