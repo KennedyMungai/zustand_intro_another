86ad64f7a65f8420956e362ebc0eb585411b6b15
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-09 12:14:57 +0300

    Added a atask to the column component

--- components/Column.tsx
+++ components/Column.tsx
@@ -1,9 +1,16 @@
+import Task from '../components/Task'
+
 type Props = {
-	state: "PLANNED" | "ONGOING" | "DONE"
+	state: 'PLANNED' | 'ONGOING' | 'DONE'
 }
 
 const Column = ({ state }: Props) => {
-	return <div className=" flex flex-1 border border-white min-h-[20rem] m-5 rounded-md p-5 w-full">{state}</div>
+	return (
+		<div className=' flex flex-1 border border-white min-h-[20rem] m-5 rounded-md p-5 w-full'>
+			<p>{state}</p>
+			<Task title='Something in the way' />
+		</div>
+	)
 }
 
 export default Column
